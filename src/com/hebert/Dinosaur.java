9cf66d32d0b3c676ba884afe4e7c854080d11ad8
hbrtxito <hbrtxito@gmail.com>
2015-09-28 13:22:55 -0500

    This is my first commit

---
+++ src/com/hebert/Dinosaur.java
@@ -0,0 +1,10 @@
+package com.hebert;
+
+/**
+ * Created by hbrtxito on 9/28/15.
+ */
+public class Dinosaur {
+    public void roar(){
+        System.out.println("GRRR!!!!!");
+    }
+}
---
+++ src/com/hebert/DinosaurSupervisor.java
@@ -0,0 +1,20 @@
+package com.hebert;
+
+public class DinosaurSupervisor {
+
+
+        public static void main(String[] args) {
+
+            Dinosaur t_rex = new Dinosaur();
+            Dinosaur velociraptor = new Dinosaur();
+            Dinosaur Diplocopus =new Dinosaur();
+
+            t_rex.roar();
+            velociraptor.roar();
+            Diplocopus.roar();
+
+            System.out.println("One more dinosaur coming ");
+
+        }
+    }
+
